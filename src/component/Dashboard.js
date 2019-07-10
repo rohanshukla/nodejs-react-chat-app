c43d17ed97930a19f63af9a51f77b801ce8df0a9
Rohan Shukla <errohanshukla@gmail.com>
2019-07-10 18:23:22 +0530

    Dashboard styling started

--- src/component/Dashboard.js
+++ src/component/Dashboard.js
@@ -1,13 +1,62 @@
-import React, { useState, Fragment } from 'react';
+import React, { useState } from 'react';
 import Grid from '@material-ui/core/Grid';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
-import Snackbar, { openSnackbar } from './utils/Snackbar';
 
 import { MESSAGE_SENT } from '../Events';
+
+const styles = theme => ({
+    root: {
+
+    },
+    container: {
+        display: 'flex',
+        margin: '0 auto',
+        height: '100vh'
+    },
+    leftContainer: {
+        flex: '1',
+        backgroundColor: "#EFEFEF",
+        [theme.breakpoints.down('sm')]: {
+            display: 'none'
+        },
+    },
+    rightContainer: {
+        flex: '4',
+        margin: '5px',
+        border: '1px #000000 solid'
+    },
+    message: {
+        fontSize: '16px',
+        margin: '5px 0',
+        [theme.breakpoints.down('sm')]: {
+            fontSize: '16px',
+        },
+    },
+    messageContainer: {
+        display: 'flex',
+        margin: '0',
+        // alignSelf: 'flex-end',
+        [theme.breakpoints.down('sm')]: {
+            width: '100%',
+        },
+    },
+    messageField: {
+        flex: '1',
+        margin: '3px',
+        padding: '5px',
+        border: '2px solid red',
+        borderRadius: '5px',
+        fontSize: '16px'
+    },
+    sendButton: {
+        margin: '3px',
+        padding: '5px',
+        border: 'none',
+        fontSize: '16px'
+    },
+});
 
 const Dashboard = (props) => {
 
@@ -19,36 +68,49 @@ const Dashboard = (props) => {
         setMessage("");
     }
 
+    const { classes } = props;
     return (
-        <Fragment>
-            <form onSubmit={onMessageSubmit}>
-                <TextField
-                    label="Message"
-                    name="message"
-                    type="text"
-                    variant="outlined"
-                    // className={classes.textField}
-                    value={message}
-                    onChange={(e) => {
-                        setMessage(e.target.value);
-                    }}
-                />
-                <br />
-                <Button type="submit" variant="outlined" color="secondary">Send</Button>
-            </form>
-
-            {
-                props.chats.map((chat, index) => {
-                    return (
-                        <div key={index}>
-                            <p>{chat.message}</p>
-                            <p>{chat.user.username}</p>
-                        </div>
-                    )
-                })
-            }
-        </Fragment >
+        <section className={classes.container}>
+            <div className={classes.leftContainer}>
+                
+            </div>
+            <div className={classes.rightContainer}>
+                <Grid container>
+                    <Grid item xs={12}>
+                        {
+                            props.chats.map((chat, index) => {
+                                return (
+                                    <div key={index}>
+                                        <Typography variant="body1" className={classes.message}>{`${chat.user.username} - ${chat.message}`}</Typography>
+                                    </div>
+                                );
+                            })
+                        }
+                    </Grid>
+                    <Grid item xs={12}>
+                        <form className={classes.messageContainer} onSubmit={onMessageSubmit}>
+                            <input
+                                name="message"
+                                type="text"
+                                variant="outlined"
+                                placeholder="Type a message"
+                                className={classes.messageField}
+                                value={message}
+                                onChange={(e) => {
+                                    setMessage(e.target.value);
+                                }}
+                            />
+                            <button type="submit" className={classes.sendButton}>Send</button>
+                        </form>
+                    </Grid>
+                </Grid>
+            </div>
+        </section >
     )
 }
 
-export default Dashboard;
+Dashboard.propTypes = {
+    classes: PropTypes.object.isRequired
+}
+
+export default withStyles(styles)(Dashboard);
